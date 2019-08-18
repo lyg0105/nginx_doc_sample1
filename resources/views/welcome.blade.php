16b56555bbeb4cda5c765e85c1b5c48640a1a089
lyg0105 <lyg0105@naver.com>
2019-08-19 05:07:28 +0900

    main world 123

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Hello World
+                    Laravel Hello World 123
                 </div>
 
                 <div class="links">
