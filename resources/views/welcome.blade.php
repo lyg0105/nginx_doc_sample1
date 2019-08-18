9a1a397e1c8aa19a6a1ba23363380f027fb133bf
lyg0105 <lyg0105@naver.com>
2019-08-19 04:48:57 +0900

    change main kk

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -93,6 +93,7 @@
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
+                <p>kk</p>
             </div>
         </div>
     </body>
